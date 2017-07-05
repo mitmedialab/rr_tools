f8fcfbbddbb9340b627926a2b3ea29bf3acc899f
Adam Gumbardo <gumbardo@mit.edu>
2017-07-05 12:41:45 -0400

    Added argparse, fixed comments, updated readme

--- README.md
+++ README.md
@@ -5,8 +5,17 @@ Tools for analysis and processing for the relational robot project.
 ## Build and Run
 
 These tools were built and tested with:
-- [ to add ]
+- ffmpeg
+- pydub (pip install, see github.com/jiaaro/pydub)
 
+## audio_cutter
+
+Python script used to cut .wav files at specified start and stop times.
+Requires a .txt file to detail this information
+'''Example:
+audio_file_name.wav	HH:MM:SS	HH:MM:SS
+audio_file_name_2.wav	HH:MM:SS	HH:MM:SS
+'''
 
 ## Reporting Bugs
 
--- audio_cutter.py
+++ audio_cutter.py
@@ -3,46 +3,56 @@
 # the directory that you want the cut audio files to be exported to [3]
 # [1] text file format: AUDIO_FILE_NAME HH:MM:SS HH:MM:SS
 
-import sys
+import argparse
 from pydub import AudioSegment
 
-def cut_audio(parsed):
-    #parsed: AUDIO_FILE START STOP
-    audio_file = parsed[0]
-    audio = AudioSegment.from_wav(in_path + audio_file)
-    cut = audio[cvt_time(parsed[1]):cvt_time(parsed[2])]
-    #create name
-    file_num = get_num(audio_file)
-    name = audio_file[0:-4] + '_cut_%d' % file_num + '.wav'
+def cut_audio(line):
+    ''' cuts the audio file at the specified start and stop times,
+    and then exports to the desiginated output folder
+    line format: [AUDIO_FILE_NAME, START, STOP]
+    '''
+    audio_file_name = line[0]
+    audio = AudioSegment.from_wav(args.in_dir + audio_file_name)
+    cut = audio[cvt_time(line[1]):cvt_time(line[2])]
+    #create name for cut audio file
+    file_num = get_num(audio_file_name)
+    cut_name = audio_file_name[0:-4] + '_cut_%d' % file_num + '.wav'
     #export to output folder
-    cut.export(out_path + name, format="wav")
+    cut.export(args.out_dir + cut_name, format="wav")
     
 def cvt_time(time):
-    #HH:MM:SS to milli-seconds
+    ''' converts time format from HH:MM:SS to milli-seconds
+    '''
     secs = int(time[0:2])*60*60 + int(time[3:5])*60 + int(time[6:])
     return secs * 1000
 
-def get_num(audio_file):
-    if audio_file in num:
-        num[audio_file] += 1
-        return num[audio_file]
+def get_num(audio_file_name):
+    ''' keeps track of how many times each audio file has been split for naming purposes
+    '''
+    if audio_file_name in num:
+        num[audio_file_name] += 1
+        return num[audio_file_name]
     else:
-        num[audio_file] = 1
-        return num[audio_file]
+        num[audio_file_name] = 1
+        return num[audio_file_name]
 
 if __name__ == '__main__':
-    #AUDIO FILE
-    file = sys.argv[1]
-    #~/Workspace/data/output/ for example
-    in_path = sys.argv[2]
-    out_path = sys.argv[3]
-    #keeps track of how many times each audio file has been split for naming purposes
+    parser = argparse.ArgumentParser(description='''Cuts .wav files at specified start 
+        and stop times''')
+    parser.add_argument('timestamps_file', help='''name of text file with timestamps, "timestamps.txt",
+        each line lists the audio file followed by the start and stop times seperated by tabs 
+        ex. my_file.wav    HH:MM:SS    HH:MM:SS''')
+    parser.add_argument('in_dir', help='directory containing audio and timestamp files')
+    parser.add_argument('out_dir', help='directory where cut .wav files will be exported')
+    
+    args = parser.parse_args()
+    
+    #num keeps track of how many times each audio file has been split for naming purposes
     num = {}
-    with open(in_path + file, 'r') as f:
+    with open(args.in_dir + args.timestamps_file, 'r') as f:
         for line in f:
-            line = line.rstrip() 
-            parsed = line.split()
-            print(parsed)
-            cut_audio(parsed)
+            line = line.rstrip().split('\t')
+            print(line)
+            cut_audio(line)
             
 
