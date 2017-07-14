20eb76ee977bf6a62d8d6594b6e80995d39a3c7b
jakory <jakory@media.mit.edu>
2017-07-14 14:30:08 -0400

    Add script to use ffmpeg to record from a camera
    - Asks user to pick video/audio devices to record from.
    - Records to a file using ffmpeg.

---
+++ record_from_camera.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Ask user to pick video and audio devices to record from.
+# Use ffmpeg to record to a file, which by default gets saved in a "rr1-videos"
+# directory in the current working directory.
+
+# If any command fails, exit the script.
+set -e
+
+# Directory to save videos in.
+VID_DIR="rr1-videos"
+
+disp() {
+    printf "\033[32m$1\n\033[39m"
+}
+
+# We need one argument: the participant ID (used in video filenames).
+if [ $# -ne 1 ]; then
+    disp "Usage: $0 <participant_id>"
+    exit 1
+fi
+
+# List devices.
+ffmpeg -f avfoundation -list_devices true -i "" 2>&1 \
+    | grep "input device" \
+    | cut -d " " -f 6-
+
+# Prompt user to pick a device.
+disp "Look at the list of devices above. Enter the index of the camera to use"
+disp "and index of the audio to use, separated by a :. Example: 0:1"
+read DEVICE1
+
+# Make a directory to save the videos in.
+mkdir -p $VID_DIR
+
+DATE="$(date +%Y-%m-%d-%H-%M-%S)"
+OUT1="$VID_DIR/rr1-$1-$DATE.mp4"
+disp "Video will be saved to: $OUT1"
+
+
+# Record using ffmpeg.
+ffmpeg -y -f avfoundation -s 640x480 -r 30 -i $DEVICE1 \
+    -vf "drawbox=y=0: color=black@1.0: width=iw:height=40: t=20, \
+        drawtext=fontfile=/Library/Fonts/Arial.ttf:  \
+        text='%{frame_num}  %{gmtime}  %{pts}': fontcolor=white: x=7: y=15: \
+        box=1: boxcolor=black@1.0" \
+    $OUT1
+
+disp "Video was saved to: $OUT1"
+
+
+
