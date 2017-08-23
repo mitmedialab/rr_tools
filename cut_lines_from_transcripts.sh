b5f18bdcb370918bb47f8a2d4d5e473617952d08
jakory <jakory@media.mit.edu>
2017-08-22 20:08:14 -0400

    Add shell script to cut child-only lines from transcripts
    Assumes a particular transcript format, namely, that there are "Tega",
    "Experimenter", and "Child" lines, and that you want to remove all
    "Tega" and "Experimenter" lines, and remove the word "Child".
    
    Includes an example for running the script on a lot of files in fish.

---
+++ cut_lines_from_transcripts.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# For the provided file:
+# - Remove "Tega" and "Experimenter" lines from transcripts.
+# - Remove the word "Child:" and any stuff in [brackets].
+#
+# To run on a lot of files in fish:
+# for file in *.txt
+#     ./cut_lines_from_transcripts.sh $file > outdir/(basename $file)
+# end
+#
+
+grep -h -v -e "^Tega" $1 | grep -v -e "^Experimenter" | sed -e "s/^Child://" -e "s/\[.*\]//g"
